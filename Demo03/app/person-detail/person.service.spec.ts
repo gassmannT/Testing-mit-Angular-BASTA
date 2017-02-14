b8d6b6758162e03bdf1ecfc1c54341790b9a6b5d
Thomas Gassmann <thomas.gassmann@windowslive.com>
2017-02-14 08:26:21 +0100

    MockBackend

--- Demo03/app/person-detail/person.service.spec.ts
+++ Demo03/app/person-detail/person.service.spec.ts
@@ -1,8 +1,9 @@
+import { Person } from './../../../Demo02/app/person-detail/person';
 import { PersonService } from './person.service';
 import { TestBed, async, inject, fakeAsync, tick } from '@angular/core/testing';
 import { Injectable } from '@angular/core';
 import { Http, HttpModule, XHRBackend, Response, ResponseOptions } from '@angular/http';
-import {MockBackend, MockConnection} from '@angular/http/testing';
+import { MockBackend, MockConnection } from '@angular/http/testing';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
@@ -21,14 +22,15 @@ describe('Person Service', () => {
             ]
         });
 
+        service = TestBed.get(PersonService);
         mockBackend = TestBed.get(MockBackend);
     });
     // beforeEach(() => {
     //     service = TestBed.get(PersonService);
     // })
-    beforeEach(inject([PersonService], (s: PersonService) => {
-        service = s;
-    }));
+    // beforeEach(inject([PersonService], (s: PersonService) => {
+    //     service = s;
+    // }));
 
     it('Get a person', () => {
         expect(service.getPerson(1).id).toEqual(1);
@@ -55,21 +57,86 @@ describe('Person Service', () => {
         expect(p).toBeDefined();
     }));
 
-    it('Should return the json of persons', async(() => {
-        mockBackend.connections.subscribe((c: MockConnection) => {
-            c.mockRespond(new Response
-                    (new ResponseOptions(`{
+    // fit('Should return the json of persons', inject([XHRBackend, PersonService],
+    //     (backend: MockBackend,
+    //         service: PersonService) => {
+    //         // Arrange
+    //         let items: Person[] = null;
+    //         backend.connections.subscribe((c: MockConnection) => {
+    //             console.log(c.request.url);
+    //             expect(c.request.url).toEqual('app/person-detail/person-data.json');
+    //             c.mockRespond(new Response
+    //                 (new ResponseOptions({ body: `[{
+    //                     "id": 4,
+    //                     "firstname": "Thomas",
+    //                     "lastname": "Gassmann",
+    //                     "twitterhandle": "gassmannT"
+    //                 }]`}))
+    //             );
+    //         });
+
+    //         // Act
+    //         service.getPersons().subscribe((data) => {
+    //             items = data;
+    //         });
+
+    //         // Assert
+    //         backend.verifyNoPendingRequests();
+    //         expect(items[0].firstname).toEqual("Thomas");
+    //     }));
+
+
+
+
+
+
+
+    fit('Should return the json of persons', async(() => {
+            // Arrange
+            let items: Person[] = null;
+            mockBackend.connections.subscribe((c: MockConnection) => {
+                console.log(c.request.url);
+                expect(c.request.url).toEqual('app/person-detail/person-data.json');
+                c.mockRespond(new Response
+                    (new ResponseOptions({
+                        body: `[{
                         "id": 4,
                         "firstname": "Thomas",
                         "lastname": "Gassmann",
                         "twitterhandle": "gassmannT"
-                    }`))
+                    }]`}))
                 );
-        });
+            });
 
-        service.getPersons().subscribe((data) => {
-            console.log("test");
-            expect(data[0].firstname).toBe('Thomas1');
-        });
-    }));
+            // Act
+            service.getPersons().subscribe((data) => {
+                items = data;
+            });
+
+            // Assert
+            mockBackend.verifyNoPendingRequests();
+            expect(items[0].firstname).toEqual("Thomas");
+        }));
+
+
+
+
+
+    // it('Should return the json of persons', async(() => {
+    //     mockBackend.connections.subscribe((c: MockConnection) => {
+    //         c.mockRespond(new Response
+    //             (new ResponseOptions(`{
+    //                     "id": 4,
+    //                     "firstname": "Thomas",
+    //                     "lastname": "Gassmann",
+    //                     "twitterhandle": "gassmannT"
+    //                 }`))
+    //         );
+    //     });
+
+    //     service.getPersons().subscribe((data) => {
+    //         console.log(data);
+    //         expect(data[0].firstname).toBe('Thomas1');
+    //     });
+    // }));
 });
