d9fdff623132e8d3579b0c46fbeb16f1817e0d94
Thomas Bandixen <thomas.bandixen@trivadis.com>
2017-02-16 12:11:19 +0100

    formatted

--- Demo01/src/calculatorModel.js
+++ Demo01/src/calculatorModel.js
@@ -1,8 +1,8 @@
-var Calculator = function() {
+var Calculator = function () {
 
 };
 
-Calculator.prototype.add = function(a, b) {
+Calculator.prototype.add = function (a, b) {
   if (a === 0) return b;
 
   if (b === 0) return a;
@@ -10,18 +10,18 @@ Calculator.prototype.add = function(a, b) {
   return a + b;
 };
 
-Calculator.prototype.multiply = function(a, b) {
+Calculator.prototype.multiply = function (a, b) {
   if (a === 0 || b === 0) {
     return 0;
   }
   return a * b;
 };
 
-Calculator.prototype.subtract = function(a, b) {
+Calculator.prototype.subtract = function (a, b) {
   return a - b;
 };
 
-Calculator.prototype.divide = function(a, b) {
+Calculator.prototype.divide = function (a, b) {
   if (b === 0) throw new Error('Attempt to divide by zero');
   return a / b;
 };
