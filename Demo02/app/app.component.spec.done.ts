2e70704743b762839370f912de6fe763a4a2e1fb
Thomas Bandixen <thomas.bandixen@trivadis.com>
2017-02-16 13:48:02 +0100

    done files

---
+++ Demo02/app/app.component.spec.done.ts
@@ -0,0 +1,20 @@
+// import { AppComponent } from './app.component';
+
+// describe('AppComponent', () => { //=> Test Suite
+
+//     let comp: AppComponent;
+
+//     beforeEach(() => {
+//         comp = new AppComponent();
+//     });
+
+//     it("is defined", () => { //=> Spec
+//         expect(comp).toBeTruthy();
+//     });
+
+//     it("title defined", () => {
+//         comp.ngOnInit();
+//         expect(comp.title).toBe("hallo basta");
+//     });
+
+// });
