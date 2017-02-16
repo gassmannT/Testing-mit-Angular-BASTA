f8bbc14525ac42c6a71c6dd9b5d30d801b2800fc
Thomas Bandixen <thomas.bandixen@trivadis.com>
2017-02-16 15:46:02 +0100

    renamed

---
+++ Demo03/app/person-detail/person-detail.component.spec.done.ts
@@ -0,0 +1,36 @@
+// import { FormsModule } from '@angular/forms';
+// import { PersonDetailComponent } from './person-detail.component';
+// import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
+
+// describe('Person Detail Component', () => {
+//     let comp: PersonDetailComponent;
+//     let fixture: ComponentFixture<PersonDetailComponent>;
+
+//     beforeEach(async(() => {
+//         TestBed.configureTestingModule({
+//             imports: [FormsModule],
+//             declarations: [PersonDetailComponent]
+//         }).compileComponents();
+//     }));
+
+//     beforeEach(() => {
+//         fixture = TestBed.createComponent(PersonDetailComponent);
+//         comp = fixture.componentInstance;
+//         fixture.detectChanges(); //triggers a change detection cycle
+
+//     });
+
+//     it("should render the title correctly", fakeAsync(() => {
+//         expect(fixture.nativeElement.querySelector('h2').innerHTML).toBe("PersonDetail: ");
+//         comp.person = {
+//             id: 1,
+//             firstname: "Thomas"
+//         }
+//         fixture.detectChanges();
+//         fixture.whenStable().then(() => {
+//             expect(fixture.nativeElement.querySelector('h2').innerHTML).toBe("PersonDetail: Thomas");
+//         });
+//     }));
+
+
+// });
