a00aa5a009404192a5f1f4ca20cbbb915178fcad
Thomas Bandixen <thomas.bandixen@trivadis.com>
2017-02-16 15:34:32 +0100

    formatted

--- Demo01/src/calculatorView.js
+++ Demo01/src/calculatorView.js
@@ -1,4 +1,4 @@
-var initCalculator = function() {
+var initCalculator = function () {
   // Get all the keys from document
   var keys = document.querySelectorAll('#calculator span');
   var operators = ['+', '-', 'x', '÷'];
@@ -6,7 +6,7 @@ var initCalculator = function() {
 
   // Add onclick event to all the keys and perform operations
   for (var i = 0; i < keys.length; i++) {
-    keys[i].onclick = function(e) {
+    keys[i].onclick = function (e) {
       // Get the input and button values
       var input = document.querySelector('.screen');
       var inputVal = input.innerHTML;
