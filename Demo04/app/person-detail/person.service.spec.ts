8ac462bd5f0c3990bb19904318b197e96404aa16
Thomas Bandixen <thomas@bandixen.ch>
2017-02-22 14:14:00 +0100

    demo4

--- Demo04/app/person-detail/person.service.spec.ts
+++ Demo04/app/person-detail/person.service.spec.ts
@@ -1,24 +1,72 @@
-import { HttpModule } from '@angular/http';
-import { async, TestBed } from '@angular/core/testing';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { HttpModule, Response, ResponseOptions, XHRBackend } from '@angular/http';
+import { async, fakeAsync, inject, TestBed, tick } from '@angular/core/testing';
 import { PersonService } from './person.service';
 
 describe("PersonService", () => {
     let service: PersonService;
+    let mockBackend: MockBackend;
+
+    // beforeEach(() => {
+    //     service = new PersonService();
+    // })
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [HttpModule],
-            providers: [PersonService]
+            providers: [PersonService, MockBackend, { provide: XHRBackend, useClass: MockBackend }]
         });
-        service = TestBed.get(PersonService);
     });
 
-    it("should get a person", () => {
-        expect(service.getPerson(1).id).toBe(1);
+    // beforeEach(inject([PersonService], (s: PersonService) => {
+    //     service = s;
+    // }));
+
+    beforeEach(() => {
+        service = TestBed.get(PersonService);
+        mockBackend = TestBed.get(XHRBackend);
     });
-    it("should get a person", async(() => {
-        service.getPersonAsync(1).then((person) => {
-            expect(person.id).toBe(1);
+
+    it("Should get a person", () => {
+        expect(service.getPerson(1).id).toEqual(1);
+        expect(service.getPerson(1).firstname).toEqual("Thomas");
+    });
+
+    it("should get a person async", async(() => {
+        service.getPersonAsync(1).then((p) => {
+            expect(p.firstname).toBe("Thomas");
         });
     }));
+
+    it("should get a person fakeAsync", fakeAsync(() => {
+        let person;
+        service.getPersonAsync(1).then((p) => {
+            person = p;
+        });
+
+        expect(person).not.toBeDefined();
+
+        tick(50);
+        expect(person).not.toBeDefined();
+
+        tick(50);
+        expect(person).toBeDefined();
+
+    }));
+
+    it("should get all persons by observables", async(() => {
+        mockBackend.connections.subscribe((c: MockConnection) => {
+            expect(c.request.url).toContain("/person-data.json");
+
+            c.mockRespond(new Response(new ResponseOptions({
+                body: [{ firstname: "Thomas" }]
+            })));
+        });
+
+        service.getPersons().subscribe((persons) => {
+            mockBackend.verifyNoPendingRequests();
+            expect(persons[0].firstname).toBe("Thomas");
+        });
+    }));
+
 });
